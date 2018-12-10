e8f308ad50ce3a5a9879514cb921575895059dd7
troy Karkula <troykark@gmail.com>
2018-12-10 14:46:39 -0600

    DB Stuff

--- worlddbinit.py
+++ worlddbinit.py
@@ -2,7 +2,7 @@ import sqlite3
 conn = sqlite3.connect('.\world.db')
 c = conn.cursor()
 #c.execute('''CREATE TABLE tile (xcoords, ycoords, elevation, terrain)''')
-c.execute('''INSERT INTO tile (xcoords, ycoords, elevation, terrain) VALUES (2,3,12,"Mountain")''')
+c.execute('''INSERT INTO tile (xcoords, ycoords, elevation, terrain) VALUES (2,3,12,"Beach")''')
 for row in c.execute("SELECT * FROM tile"):
     print row
 conn.commit()
