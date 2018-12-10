8b8f41e140e9256e1d9f8664bea0217c5cb54537
troy Karkula <troykark@gmail.com>
2018-12-10 14:28:20 -0600

    Database crap

--- worlddbinit.py
+++ worlddbinit.py
@@ -1,6 +1,9 @@
 import sqlite3
 conn = sqlite3.connect('.\world.db')
 c = conn.cursor()
-c.execute('''
-    CREATE TABLE tile (xcoords, ycoords, elevation, terrain)
-''')
+#c.execute('''CREATE TABLE tile (xcoords, ycoords, elevation, terrain)''')
+c.execute('''INSERT INTO tile (xcoords, ycoords, elevation, terrain) VALUES (2,3,12,"Mountain")''')
+for row in c.execute("SELECT * FROM tile"):
+    print row
+conn.commit()
+conn.close()
