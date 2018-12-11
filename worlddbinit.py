56ca1a1537978f128f554558ffcc312b6101ebaa
troy Karkula <troykark@gmail.com>
2018-12-11 11:55:37 -0600

    Added insert function

--- worlddbinit.py
+++ worlddbinit.py
@@ -1,11 +1,15 @@
 import sqlite3, random
 conn = sqlite3.connect('.\world.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE tile (xcoords, ycoords, elevation, terrain)''')
-c.execute('''INSERT INTO tile (xcoords, ycoords, elevation, terrain) VALUES (2,3,12,"Beach")''')
+
+def createDB():
+    c.execute('''CREATE TABLE tile (xcoords, ycoords, elevation, terrain, chunk)''')
+def inserttile(xcord, ycord, elevation, terrain, chunk):
+    c.execute('INSERT INTO tile (xcoords, ycoords, elevation, terrain, chunk) VALUES ('+ str(xcord) +','+ str(ycord) + ','+ str(elevation) + ',"' + terrain + '",' +  chunk+ ')')
+createDB()
 for cx in range(0,300):
     for cy in range(0,300):
-         c.execute('INSERT INTO tile (xcoords, ycoords, elevation, terrain) VALUES ('+ str(cx) +','+ str(cy) + ','+ str(random.random() *10) + ',"null")')
+         inserttile(cx,cy,random.randrange(0,30),random.choice(["mountain","plains","ocean"]),str(1))
 for row in c.execute("SELECT * FROM tile"):
     print row
 conn.commit()
