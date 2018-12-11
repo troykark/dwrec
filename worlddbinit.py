dac65181c1866c361021096412b789d431c4e062
troy Karkula <troykark@gmail.com>
2018-12-11 08:43:06 -0600

    added world insert

--- worlddbinit.py
+++ worlddbinit.py
@@ -1,8 +1,11 @@
-import sqlite3
+import sqlite3, random
 conn = sqlite3.connect('.\world.db')
 c = conn.cursor()
-#c.execute('''CREATE TABLE tile (xcoords, ycoords, elevation, terrain)''')
+c.execute('''CREATE TABLE tile (xcoords, ycoords, elevation, terrain)''')
 c.execute('''INSERT INTO tile (xcoords, ycoords, elevation, terrain) VALUES (2,3,12,"Beach")''')
+for cx in range(0,300):
+    for cy in range(0,300):
+         c.execute('INSERT INTO tile (xcoords, ycoords, elevation, terrain) VALUES ('+ str(cx) +','+ str(cy) + ','+ str(random.random() *10) + ',"null")')
 for row in c.execute("SELECT * FROM tile"):
     print row
 conn.commit()
